d21560f96b9b92e62a664ec47a7c769011ec55c0
LeleDerGrasshalmi <king-of-gosts@web.de>
2024-12-28 15:46:59 +0100

    wipe code

--- src/types/content-pages.ts
+++
@@ -1,7 +0,0 @@
-export type ContentPagesData = Record<string, ContentPagesValue>;
-
-export interface ContentPagesSection {
-  _title?: string;
-}
-
-export type ContentPagesValue = unknown[] | unknown | ContentPagesSection;
